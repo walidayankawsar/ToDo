ca07c920e6d1f7694b8b116cbb6b2a754fa30b8a
walidayankawsar <walidayankawsar@gmail.com>
2025-07-10 08:32:43 -0700

    updated

--- base/views.py
+++ base/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.views.generic.list import ListView
 from .models import Task
 
@@ -7,3 +6,4 @@ from .models import Task
 # Create your views here.
 class TaskList(ListView):
     model = Task
+    context_object_name = "tasks"
