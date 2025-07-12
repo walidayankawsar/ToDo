f4fdb8eb51d45b83b3402634c99636439d555bf6
walidayankawsar <walidayankawsar@gmail.com>
2025-07-12 05:54:47 -0700

    added get_context_data

--- base/views.py
+++ base/views.py
@@ -29,7 +29,8 @@ class TaskList(LoginRequiredMixin, ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['color'] = 'red'
+        context['tasks'] = context['tasks'].filter(user=self.request.user)
+        context['count'] =context['tasks'].filter(complete=False).count()
         return context
 
 
