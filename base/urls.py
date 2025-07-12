1f37453c7fed5f81d9405cca7aaadc1fd7ef8c1e
walidayankawsar <walidayankawsar@gmail.com>
2025-07-12 01:55:40 -0700

    added path in urls.py

--- base/urls.py
+++ base/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, DeleteView
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, DeleteView, CustomLoginView
 
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
@@ -7,4 +7,5 @@ urlpatterns = [
     path('task-create/', TaskCreate.as_view(), name='task-create'),
     path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task-update'),
     path('task-delete/<int:pk>/', DeleteView.as_view(), name='task-delete'),
+    path('login/', CustomLoginView, name='login'),
 ]
