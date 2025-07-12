7099255750c8eec3214fc45caf634e3ef2b5847e
walidayankawsar <walidayankawsar@gmail.com>
2025-07-12 04:13:23 -0700

    added logout option

--- base/urls.py
+++ base/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, DeleteView, CustomLoginView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
@@ -8,4 +9,5 @@ urlpatterns = [
     path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task-update'),
     path('task-delete/<int:pk>/', DeleteView.as_view(), name='task-delete'),
     path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
 ]
