0b7d038e9a2dcb7979e1068c6b0fa407d7d81e24
walidayankawsar <walidayankawsar@gmail.com>
2025-07-21 09:47:42 -0700

    added search button

--- base/views.py
+++ base/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
@@ -36,6 +36,11 @@ class RegisterPage(FormView):
         if user is not None:
             login(self.request, user)
         return super(RegisterPage, self).form_valid(form)
+    
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect ('tasks')
+        return super(RegisterPage, self).get(*args, **kwargs)
 
 
 
