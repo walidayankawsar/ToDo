ee52d1b41cfb0cdc82f42142cfdeb45f4f2a08a6
walidayankawsar <walidayankawsar@gmail.com>
2025-07-21 10:24:07 -0700

    updated

--- base/views.py
+++ base/views.py
@@ -54,7 +54,7 @@ class TaskList(LoginRequiredMixin, ListView):
         context['count'] =context['tasks'].filter(complete=False).count()
         search_input = self.request.GET.get('search-area') or ''
         if search_input:
-            context['tasks'] = context['tasks'].filter(title__icontains=search_input)
+            context['tasks'] = context['tasks'].filter(title__startswith=search_input)
         context['search_input'] = search_input
         return context
 
