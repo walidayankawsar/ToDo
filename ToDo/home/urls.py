67739dddffeff684a96b4dca9f47f185b0e79e49
walidayankawsar <walidayankawsar@gmail.com>
2025-09-29 06:31:40 -0700

    created

---
+++ ToDo/home/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.user_login, name='login'),
+    path('home/', views.home, name='home'),
+    path('logout/', views.user_logout, name='logout'),
+]
