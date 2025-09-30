336d86b0da054b0ea19fe84312c5cf6d4f7d3ce9
walidayankawsar <walidayankawsar@gmail.com>
2025-09-30 08:03:33 -0700

    created views for registeration

--- ToDo/home/views.py
+++ ToDo/home/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 def user_login(request):
     error = None   # শুরুতে error নাই
@@ -17,9 +19,44 @@ def user_login(request):
             return redirect("home")
         else:
             error = "Username or password ভুল!" 
-            print("❌ Error set হলো:", error)
 
     return render(request, "registration/login.html", {"error": error})
+
+def register(request):
+
+    if request.method == 'POST':
+        # getting user inputs from frontend
+        first_name = request.POST.get('first name')
+        last_name = request.POST.get('last name')
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        user_data_has_error = False
+
+        if User.objects.filter(username=username).exists():
+            user_data_has_error = True
+            messages.error(request, 'Username already exists..')
+
+        if User.objects.filter(email=email).exists():
+            user_data_has_error = True
+            messages.error(request, 'Email already exists..')
+
+        if not user_data_has_error:
+            new_user = User.objects.create_user(
+                first_name = first_name,
+                last_name = last_name,
+                email = email,
+                username = username,
+                password = password,
+            )
+            messages.success(request,'Account created. Login now..')
+        else:
+            return redirect('register')
+
+
+
+    return render(request, 'register.html')
 
 
 @login_required
