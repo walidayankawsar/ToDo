d6e7835e100a87d03c588fbcb654d3a7db609baf
walidayankawsar <walidayankawsar@gmail.com>
2025-10-01 03:10:18 -0700

    register path added

--- ToDo/home/urls.py
+++ ToDo/home/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.user_login, name='login'),
     path('home/', views.home, name='home'),
-    path('logout/', views.user_logout, name='logout'),
     path('register/', views.register, name='register'),
+    path('logout/', views.logoutview, name='logout'),
+    path('reset/', views.reset, name='reset'),
 ]
