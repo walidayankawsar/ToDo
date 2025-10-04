8be69d886095379158c0db7b4e078539234b7ebc
walidayankawsar <walidayankawsar@gmail.com>
2025-10-04 00:56:47 -0700

    creating models for pass reset

--- ToDo/home/models.py
+++ ToDo/home/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
 
 # Create your models here.
+
+class PasswordReset(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    reset_id = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
+    created_when = models.DateField(auto_now_add=True)
+
+
