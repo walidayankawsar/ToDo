4dcb66158b640a1a7819fa7bb99dd2bc6ebc78cb
walidayankawsar <walidayankawsar@gmail.com>
2025-11-14 03:39:31 -0800

    updated home, created toggle task, delete task

--- home/views.py
+++ home/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib import messages
-from .models import PasswordReset
+from .models import PasswordReset, Task
 from django.urls import reverse
 from django.core.mail import EmailMessage
 from django.conf import settings
@@ -143,9 +143,65 @@ def new_pass(request, reset_id):
 
 @login_required
 def home(request):
-    return render(request, "ToDo.html")
+
+    filter_type = request.GET.get('filter', 'all')
+
+    if filter_type == 'completed':
+        tasks = Task.objects.filter(user=request.user, is_completed=True)
+    elif filter_type == 'imcompleted':
+        tasks = Task.objects.filter(user=request.user, is_completed=False)
+    elif filter_type == 'important':
+        tasks = Task.objects.filter(user=request.user, priority='high')
+    else:
+        tasks = Task.objects.filter(user=request.user)
 
 
+
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        priority = request.POST.get('priority')
+
+        if title:
+            Task.objects.create(user=request.user, title=title, priority=priority)
+            messages.success(request, "নতুন কাজ সফলভাবে যোগ হয়েছে!")
+            return redirect('home')
+        
+    completed = Task.objects.filter(user=request.user, is_completed=True).count()
+    total = Task.objects.filter(user=request.user).count()
+    remaining = completed - total
+
+    context = {
+        "completed_task": completed,
+        "total_task": total,
+        "remaining_task": remaining,
+        "filter_type": filter_type,
+        "tasks": tasks.order_by('-created_at'),
+    }
+        
+    return render(request, "ToDo.html", context)
+
+@login_required
+def toggle_task(request, task_id):
+    task = Task.objects.get(user=request.user, id=task_id)
+    task.is_completed = not task.is_completed
+    task.save()
+    return redirect('home')
+
+@login_required
+def delete_task(request, task_id):
+    task = Task.objects.get(user=request.user, id=task_id)
+    task.delete()
+    messages.success(request, "কাজটি সফলভাবে মুছে ফেলা হয়েছে।")
+    return redirect('home')
+
+@login_required
+def delete_completed_tasks(request, task_id):
+    task = Task.objects.get(user=request.user, is_completed=True)
+    task.deleted()
+    messages.success(request, "সম্পূর্ণ কাজগুলো মুছে ফেলা হয়েছে।")
+    return redirect('home')
+
+@login_required
 def logoutview(request):
     logout(request)
     return redirect('login')
