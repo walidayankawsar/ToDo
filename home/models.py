227a625d4e0482ccb149330f80a120f637e5a6fd
walidayankawsar <walidayankawsar@gmail.com>
2025-11-14 03:37:32 -0800

    task model created

--- home/models.py
+++ home/models.py
@@ -9,4 +9,15 @@ class PasswordReset(models.Model):
     reset_id = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
     created_when = models.DateTimeField(auto_now_add=True)
 
+class Task(models.Model):
+    priority_list = [
+        ("low", "কম গুরুত্বপূর্ণ"),
+        ("medium", "মাঝারি"),
+        ("high", "গুরুত্বপূর্ণ"),
+    ] 
 
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=260)
+    is_completed = models.BooleanField(default=False)
+    priority = models.CharField(max_length=10, choices=priority_list, default="medium")
+    created_at = models.DateTimeField(auto_now_add=True)
