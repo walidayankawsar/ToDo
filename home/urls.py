90126554d795488a3c6b0fe9c42243df95eccba9
walidayankawsar <walidayankawsar@gmail.com>
2025-11-15 08:21:21 -0800

    task add and delete urls added

--- home/urls.py
+++ home/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
     path('reset/', views.reset, name='reset'),
     path('message/<str:reset_id>/', views.reset_message, name='message'),
     path('new_password/<str:reset_id>/', views.new_pass, name='newpass'),
+
+    path('toggle/<int:task_id>/', views.toggle_task, name='toggle_task'),
+    path('delete/<int:task_id>/', views.delete_task, name='delete_task'),
+    path('delete_completed/', views.delete_completed_tasks, name='delete_completed'),
 ]
